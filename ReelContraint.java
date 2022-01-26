c8d7801016db9cd329e739011fd79b0217eb50ef
zbendd <zahira.benddib@gmail.com>
2022-01-26 23:42:53 +0100

    add new class

---
+++ ReelContraint.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.td2bisproject;
+
+/**
+ *
+ * @author etulyon1
+ */
+public class ReelContraint {
+    //voyage = villeDepart,
+                //villeArrivee,
+                // longueur( entre 1 et 30000),
+                // prixAuKm(entre 0,05 et 3.2)
+     
+    private int val;
+}
